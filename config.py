31dbc4b9d3b981884c869a2b7cdb36875e7e6deb
Ivan <nexacoders2@gmail.com>
2026-08-07 12:13:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,3 +3,5 @@ API_HASH = "d6186691704bd901bdab275ceaab88f3"
 BOT_TOKEN = "8720521111:AAE9mG28kqhnffHkFeVPTgXYFTLOrZpWVAo"
 
 MONGO_URI = "mongodb+srv://nexacoders2_db_user:dxYh7QOdHvH6OVdd@cluster0.f4qxcbk.mongodb.net/?appName=Cluster0"
+
+OWNER_ID = 8694029886
