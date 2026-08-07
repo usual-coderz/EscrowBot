4a2923111d6d758f06917706f74ff56c865b1e3a
Ivan <nexacoders2@gmail.com>
2026-08-07 11:01:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-API_ID = 12345678
-API_HASH = "YOUR_API_HASH"
-BOT_TOKEN = "YOUR_BOT_TOKEN"
+API_ID = 22657083
+API_HASH = "d6186691704bd901bdab275ceaab88f3"
+BOT_TOKEN = "8720521111:AAE9mG28kqhnffHkFeVPTgXYFTLOrZpWVAo"
 
-MONGO_URI = "mongodb://localhost:27017"
+MONGO_URI = "mongodb+srv://nexacoders2_db_user:dxYh7QOdHvH6OVdd@cluster0.f4qxcbk.mongodb.net/?appName=Cluster0"
