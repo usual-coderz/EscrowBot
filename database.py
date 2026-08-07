81f3dd764e7cd803181903fd2459e95e99c3622a
Ivan <nexacoders2@gmail.com>
2026-08-07 21:14:14 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,14 +1,14 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 from config import MONGO_URI
 
-client = AsyncIOMotorClient(MONGO_URI)
+mongo = AsyncIOMotorClient(MONGO_URI)
 
-db = client["escrowbot"]
+db = mongo["escrowbot"]
 
-# Collections
 admins = db["admins"]
 deals = db["deals"]
 users = db["users"]
 trades = db["trades"]
 payments = db["payments"]
 warnings = db["warnings"]
+settings = db["settings"]
